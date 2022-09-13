5c9324ad90ffc63433c922559e0359730045bd2d
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-09-13 18:37:35 -0300

    Material Colours altered; pickups added to 32

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,7 +44,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 32)
         {
             winTextObject.SetActive(true);
         }
